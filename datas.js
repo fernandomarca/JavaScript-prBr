784a8ecf07ce4f233d30a941aad906184b3de9b5
fernandomarca <fernandomarca@hotmail.com>
2020-10-24 20:51:45 -0300

    datas

---
+++ datas.js
@@ -0,0 +1,12 @@
+var d = Date.now();
+
+var d2 = new Date(1603583023446)
+
+var dias = [
+    "Dom", "Seg", "Ter", "Qua", "Quin", "Sex", "Sab"
+];
+
+//console.log(dias[d.getDay()]);
+console.log(d);
+console.log(d2);
+
