cc58a1d633aae39996657d487fef8a062f98dac5
fernandomarca <fernandomarca@hotmail.com>
2020-10-25 12:50:06 -0300

    math

---
+++ math.js
@@ -0,0 +1,11 @@
+let pi = Math.PI;
+console.log(pi);
+
+let sqrt = Math.sqrt(16);
+console.log(sqrt);
+
+let pow = Math.pow(2, 8);
+console.log(pow);
+
+let random = Math.random();
+console.log(random);
